c8e85577cf005839c03a6a2295de8874ad487f5d
Bojin Yao <bojinyao@berkeley.edu>
2020-10-07 02:01:15 -0700

    add argparse as dependency

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,13 @@ export interface ConfigInfo {
     msgSelfDeleteMilSec: number,                // Number of milliseconds before a message self destructs
     channels: Record<string, TextChannelInfo>   // Mappings between Channel ID: string --> TextChannelInfo
     rolesNoMod?: string[]                       // Discord Roles that are not moderated by the Bot
+    rolesCanCmd?: string[]                      // Discord Roles that can command Bot
 }
 
 export interface TextChannelInfo {
     calendarId: string,                         // Required public Google Calendar ID
     name?: string,                              // Name of the channel (for printing purposes only)
-    msgSelfDeleteMilSec?: number,               // Override msgSelfDeleteMilSec in ConfigInfo
-    rolesNoMod?: string[]                       // Override rolesNoMod in ConfigInfo
+    msgSelfDeleteMilSec?: number,               // Completely Override msgSelfDeleteMilSec in ConfigInfo
+    rolesNoMod?: string[]                       // Completely Override rolesNoMod in ConfigInfo
+    rolesCanCmd?: string[]                      // Completely Override rolesCanCmd in ConfigInfo
 }
