3d991917e188fe7dc669ad43709612be627bb23b
Bojin Yao <bojinyao@berkeley.edu>
2021-01-13 20:36:03 -0800

    new channel IDs

--- bot-config.ts
+++ bot-config.ts
@@ -5,11 +5,11 @@ export const Config: ConfigInfo = {
     msgSelfDeleteMilSec: 10000,
 
     channels: {
-        "747247932033597531": {
+        "799096628115800068": {
             name: 'oh-queue',
             calendarId: 'berkeley.edu_k2g60q1sehd2u0ujd257jqm7h0@group.calendar.google.com',
         },
-        "747244970108387409": {
+        "799096627888390150": {
             name: 'lab-queue',
             calendarId: 'berkeley.edu_d358eocqj23pak3atie23vk35o@group.calendar.google.com'
         }
