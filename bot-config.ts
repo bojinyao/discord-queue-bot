472c02029676249e4190bb723233c98ac09eeca9
Max Yao <bojinyao@berkeley.edu>
2021-06-06 20:03:18 -0700

    Update bot-config.ts

--- bot-config.ts
+++ bot-config.ts
@@ -5,11 +5,11 @@ export const Config: ConfigInfo = {
     msgSelfDeleteMilSec: 10000,
 
     channels: {
-        "799096628115800068": {
+        "850148710579175489": {
             name: 'oh-queue',
             calendarId: 'berkeley.edu_k2g60q1sehd2u0ujd257jqm7h0@group.calendar.google.com',
         },
-        "799096627888390150": {
+        "850148709999181850": {
             name: 'lab-queue',
             calendarId: 'berkeley.edu_d358eocqj23pak3atie23vk35o@group.calendar.google.com'
         }
